230cd7e6dfdfc4ce2427e3f2e7b0b0bbf56c9376
Michael Skogberg <michael.skogberg@futurice.com>
2018-03-17 22:59:50 +0200

    Remove filters

--- app/com/malliina/app/AppLoader.scala
+++ app/com/malliina/app/AppLoader.scala
@@ -7,22 +7,20 @@ import controllers.{AssetsComponents, GithubProxy}
 import play.api.ApplicationLoader.Context
 import play.api.libs.ws.ahc.AhcWSComponents
 import play.api.routing.Router
-import play.api.{BuiltInComponentsFromContext, Mode}
-import play.filters.HttpFiltersComponents
-import play.filters.hosts.AllowedHostsConfig
+import play.api.{BuiltInComponentsFromContext, Mode, NoHttpFiltersComponents}
 import router.Routes
 
 class AppLoader extends DefaultApp(new AppComponents(_))
 
 class AppComponents(context: Context)
   extends BuiltInComponentsFromContext(context)
-    with HttpFiltersComponents
+    with NoHttpFiltersComponents
     with AhcWSComponents
     with AssetsComponents {
   val conf =
     if (environment.mode == Mode.Test) GithubConf("", FullUrl.build("http://www.google.com").toOption.get)
     else GithubConf.fromEnvOrFail()
   val home = new GithubProxy(conf, wsClient, controllerComponents)
-  override lazy val allowedHostsConfig = AllowedHostsConfig(Seq("ci.malliina.com", "localhost"))
+  //  override lazy val allowedHostsConfig = AllowedHostsConfig(Seq("ci.malliina.com", "localhost"))
   override val router: Router = new Routes(httpErrorHandler, home)
 }
